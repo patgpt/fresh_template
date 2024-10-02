801d9f26c829f0e98bde8a54bf941e9096e551f9
patgpt <doitformurphy@gmail.com>
2024-10-01 21:49:55 -0300

    Add Navbar component

---
+++ components/Projects.tsx
@@ -0,0 +1,9 @@
+export default function Projects() {
+    return (
+      <section>
+        <h2>My Projects</h2>
+        {/* List your projects here */}
+      </section>
+    );
+  }
+  
--- routes/index.tsx
+++ routes/index.tsx
@@ -4,19 +4,18 @@ import Counter from "../islands/Counter.tsx";
 export default function Home() {
   const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
+    <div class="h-full px-4 py-8 mx-auto bg-[#86efac]">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <img
-          class="my-6"
-          src="/logo.svg"
+          class="my-6 rounded-full shadow-2xl"
+          src="/pfp.webp"
           width="128"
           height="128"
           alt="the Fresh logo: a sliced lemon dripping with juice"
         />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+        <h1 class="text-4xl font-bold">Patrick Kelly</h1>
         <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
+          Hey there! Wave 👋 I'm Patrick Kelly This is a Fresh app. It's a Preact app with server-side rendering
         </p>
         <Counter count={count} />
       </div>
